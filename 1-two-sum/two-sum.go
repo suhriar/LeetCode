96dc9a81093773fd723273e20ce0b60573ed4b73
suhriar <79493968+suhriar@users.noreply.github.com>
2025-02-10 15:21:23 +0700

    Time: 0 ms (100.00%) | Memory: 6 MB (19.37%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,8 +1,7 @@
 func twoSum(nums []int, target int) []int {
     numMap := make(map[int]int)
 
-    for i := 0; i < len(nums); i++ {
-        num := nums[i]
+    for i, num := range nums {
         complement := target - num
         if _, found := numMap[complement]; found {
             return []int{numMap[complement], i}
