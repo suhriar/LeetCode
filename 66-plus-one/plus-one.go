b61e5c0d11d62c32ea911dfc365197be1fdf156d
suhriar <79493968+suhriar@users.noreply.github.com>
2025-04-20 10:07:24 +0700

    Time: 0 ms (100.00%) | Memory: 4 MB (28.61%) - LeetSync

---
+++ 66-plus-one/plus-one.go
@@ -0,0 +1,13 @@
+func plusOne(digits []int) []int {
+    n := len(digits)
+
+    for i := n - 1; i >= 0; i-- { 
+        if digits[i] < 9 {
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0 
+    }
+
+    return append([]int{1}, digits...)
+}
